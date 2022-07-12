fa3428aa20de42d34992ae044db4004c443e7549
kishore kumar <kishorejaipal477@gmail.com>
2022-07-12 12:06:51 +0500

    print natural number using for loop

--- C_basics/loops/ecercise/print_num.c
+++ C_basics/loops/ecercise/print_num.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
+// int main(){
+//     int n=0,i=1;
+//     //print  natural number 
+//     // int n = 10,i=1;
+//     printf("Enter the number:");
+//     scanf("%d",&n);
+//     while (i<n)
+//     {
+        
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+// using for loops
 int main(){
-    int n=0,i=1;
-    //print  natural number 
+    int n=10,i;
     // int n = 10,i=1;
     printf("Enter the number:");
     scanf("%d",&n);
-    while (i<n)
-    {
-        
-        printf("%d\n",i);
-        i++;
-    }
+     printf("Natural numbers from 1 to %d : \n", n);
+   for ( i = 0; i < n; i++)
+   {
+    printf("%d\n",i);
+   }
+   
     return 0;
 }
