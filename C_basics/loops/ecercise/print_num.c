2b2835aa155ce859f16c612b440432a91c99e911
kishore kumar <kishorejaipal477@gmail.com>
2022-07-12 12:01:16 +0500

    print natural numbers

---
+++ C_basics/loops/ecercise/print_num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n=0,i=1;
+    //print  natural number 
+    // int n = 10,i=1;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    while (i<n)
+    {
+        
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
