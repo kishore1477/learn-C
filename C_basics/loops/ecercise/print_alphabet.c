6e458288384c27f50ddc8f5f06709854a11ccff2
kishore kumar <kishorejaipal477@gmail.com>
2022-07-12 13:55:43 +0500

    print small alphabet letter

---
+++ C_basics/loops/ecercise/print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    // int arr[20];
+    char ch ;
+    printf("print alphabet letter from a-z\n");
+     for ( ch = 'a'; ch<='z'; ch++)
+     {
+        printf("%c\n", ch);
+     }
+     return 0;
+   
+}
