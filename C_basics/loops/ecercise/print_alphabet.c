5ea0570d09e941a4d5b6c9b6d980f9f86691dc64
kishore kumar <kishorejaipal477@gmail.com>
2022-07-12 13:56:46 +0500

    print big  alphabet letter

--- C_basics/loops/ecercise/print_alphabet.c
+++ C_basics/loops/ecercise/print_alphabet.c
@@ -3,7 +3,7 @@ int main(){
     // int arr[20];
     char ch ;
     printf("print alphabet letter from a-z\n");
-     for ( ch = 'a'; ch<='z'; ch++)
+     for ( ch = 'A'; ch<='Z'; ch++)
      {
         printf("%c\n", ch);
      }
