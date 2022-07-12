b54dc7d04c709480a3e5a9d65d541c8aa7aba32f
kishore kumar <kishorejaipal477@gmail.com>
2022-07-12 11:27:57 +0500

    sum of n odd number

--- C_basics/loops/ecercise/print_sum_of_n_number.c
+++ C_basics/loops/ecercise/print_sum_of_n_number.c
@@ -18,7 +18,30 @@ int main(){
 // printf("%d", sum);
 // // printf("The sum of natural numbers from %d to %d is %d: ",start_num, end_num, sum);
 
-    // sum of even  number
+//     // sum of even  number
+//         int start_num=0, end_num=0, sum=0;
+//         printf("\" Welcome to sum of even number portion \" \n");
+//   printf("Enter a starting number:\n");
+//   scanf("%d",&start_num);
+//   printf("Enter a Ending number:\n");
+//   scanf("%d",&end_num);
+// //   printf("The sum of %d is :\n",n);
+// printf("The sum of even numbers from %d to %d is: \n",start_num, end_num);
+
+// while (start_num<=end_num)
+// {
+//     if(start_num %2 == 0){
+// printf("%d\n",start_num);
+//     sum = sum + start_num;
+
+//     }
+//     // printf("%d + %d =  %d \n",start_num,sum);
+//     start_num++;
+// }
+// printf(" sum is %d \n", sum);
+
+
+    // sum of odd number
         int start_num=0, end_num=0, sum=0;
         printf("\" Welcome to sum of even number portion \" \n");
   printf("Enter a starting number:\n");
@@ -26,11 +49,11 @@ int main(){
   printf("Enter a Ending number:\n");
   scanf("%d",&end_num);
 //   printf("The sum of %d is :\n",n);
-printf("The sum of even numbers from %d to %d is: \n",start_num, end_num);
+printf("The sum of odd numbers from %d to %d is: \n",start_num, end_num);
 
 while (start_num<=end_num)
 {
-    if(start_num %2 == 0){
+    if(start_num %2 != 0){
 printf("%d\n",start_num);
     sum = sum + start_num;
 
@@ -38,7 +61,6 @@ printf("%d\n",start_num);
     // printf("%d + %d =  %d \n",start_num,sum);
     start_num++;
 }
-printf(" sum is %d \n", sum);
-    // sum of odd number
+printf("sum is %d :",sum );
     return 0;
 }
