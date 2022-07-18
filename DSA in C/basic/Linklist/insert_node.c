538c067876c7cb8496ded9cb0a7383a64f5ef085
kishore kumar <kishorejaipal477@gmail.com>
2022-07-18 17:18:40 +0500

    insert node

---
+++ DSA in C/basic/Linklist/insert_node.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+ int data;
+ struct Node *next;
+};
+
+void linklistTraversal(struct Node *ptr){
+   while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+    
+}
+struct Node   * insertAtfirst(struct Node *head,int data){
+    struct Node *ptr = (struct Node*) malloc(sizeof(struct Node));
+    ptr -> next = head;
+      ptr->data = data;
+        return ptr;
+
+}
+// 
+struct Node   * InsertAtIndex(struct Node *head,int data,int index){
+    struct Node *ptr = (struct Node*) malloc(sizeof(struct Node));
+    struct Node *p =head;
+    int i =0;
+
+while (i != index-1)
+{
+    p = p->next;
+    i++;
+}
+ptr->data=data;
+ptr->next = p->next;
+p->next = ptr;
+return head;
+
+}
+
+int main(){
+struct Node *head;
+struct Node *second;
+struct Node *third;
+struct Node *fourth;
+// allocate all in heap 
+head = (struct Node*) malloc(sizeof(struct Node));
+second = (struct Node*) malloc(sizeof(struct Node));
+third = (struct Node*) malloc(sizeof(struct Node));
+fourth = (struct Node*) malloc(sizeof(struct Node));
+
+//😍 link the node 
+// link first and  nodes
+head->data = 4;
+head->next=second;
+// link second and third nodes
+second->data = 6;
+second->next=third;
+// link third and fourth nodes
+third->data = 16;
+third->next=fourth;
+// Terminate nodes
+fourth->data =27;
+fourth->next=NULL;
+
+linklistTraversal(head);
+// head =insertAtfirst(head, 60);
+head =InsertAtIndex(head, 64,1);
+linklistTraversal(head);
+
+return 0;
+}
